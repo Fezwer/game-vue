daa67af2e5f9326ff3419cd627b636e4955b0cbd
Fezwer <oleg-lib@yandex.ru>
2024-06-27 10:37:01 +1000

    full_game

---
+++ src/components/Playground.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="score-area score">
+      {{ score >= 0 ? "Очки:" : "" }}
+      <code v-if="score >= 0" class="score-value">{{ score }}</code>
+    </div>
+    <div class="game-area">
+      <v-grid />
+    </div>
+    <div class="score-area scoreboard"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import VGrid from "@/components/Grid.vue";
+
+export default {
+  name: "Playground",
+  components: {
+    VGrid,
+  },
+  props: {
+    score: {
+      type: Number,
+      required: false,
+    },
+  },
+};
+</script>
+
